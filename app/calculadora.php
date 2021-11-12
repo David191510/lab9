32084c2c5e469d1a4567862743dd299a9a5ffc94
David Wu <david.wu@utp.ac.pa>
2021-11-11 23:29:56 -0500

    lab8

--- app/calculadora.php
+++ app/calculadora.php
@@ -1,6 +1,28 @@
 <?php
-class Calculadora{
-    function sumar($num1, $num2){
-        return $num1+$num2;
+    class Calculadora {
+        function sumar($x, $y){
+            return $x + $y;
+        }
+
+        function restar($x, $y){
+            return $x - $y;
+        }
+
+        function multiplicar($x, $y){
+            return $x * $y;
+        }
+        function dividir($x, $y){
+            if($y === 0){
+                throw new exception("Division por 0");
+            }
+            return $x / $y;
+        }
+        function generarArreglo(){
+            $arreglo = array();
+            for($i=0; $i<5; $i++){
+                $arreglo[$i] = mt_rand(1, 10);
+            }
+            return $arreglo;
+        }
     }
-}
+?>
